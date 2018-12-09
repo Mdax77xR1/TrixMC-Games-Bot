baa7356689788191efc1188581e8bada11e4bbfc
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2018-12-09 12:30:14 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,97 @@
 
+const Discord = require('discord.js');
+const fs = require("fs"); 
+const jimp = require('jimp');   
+var prefix = "."
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+ client.user.setActivity("",{type: 'watching'})
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Welcome Mdax`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     xR1Server' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`$help $inv`,"https://www.twitch.tv/Mdax77xR1")
+client.user.setStatus("online")
+ 
+});
+
+client.on("message", async message => {
+var trans =[
+    {q:"**ما معنى `Train` ?**",a:"قطار"},
+    {q:"**ما معنى `Plane` ?**",a:"طائرة"},
+    {q:"**ما معنى `Home` ?**",a:"منزل"},
+    {q:"**ما معنى `Cook` ?**",a:"يطبخ"},
+    {q:"**ما معنى `Car` ?**",a:"سيارة"},
+    {q:"**ما معنى `Ask` ?**",a:"سؤال"},
+    {q:"**ما معنى `afraid` ?**",a:"خائف"},
+    {q:"**ما معنى `Air` ?**",a:"هواء"},
+    {q:"**ما معنى `Always` ?**",a:"دائما"},
+    {q:"**ما معنى `Never` ?**",a:"ابدا"},
+    {q:"**ما معنى `box` ?**",a:"صندوق"},
+
+
+];
+    if(message.content == prefix+"ترجمة"){
+        if(UserBlocked.has(message.guild.id)) return message.channel.send("هناك جلسة .")
+        UserBlocked.add(message.guild.id)
+        var ask = trans[Math.floor(Math.random() * trans.length)];
+        let embed = new Discord.RichEmbed()
+        .setTitle('سؤال ترجمة')
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setColor("RANDOM")
+        .setDescription(ask.q);
+        message.channel.sendEmbed(embed).then(msg=> msg.delete(20000))
+        const msgs = await message.channel.awaitMessages(msg => msg.author.id !== client.user.id ,{maxMatches:1,time:10000});
+            UserBlocked.delete(message.guild.id)
+        msgs.forEach(result => {
+           if(result.author.id == client.user.id) return;
+           if(result.content == "ترجمة") return
+           if(result.content == ask.a){
+             let embeds = new Discord.RichEmbed()
+             .setTitle(':white_check_mark: اجابة صحيحة')
+             .setAuthor(message.author.username, message.author.avatarURL)
+             .setColor("RANDOM")
+             .setDescription(`**${result.author.username}** الإجابة صحيحة`);
+                message.channel.sendEmbed(embeds);                return;
+           } else {
+ 
+                                  var embedx = new Discord.RichEmbed()
+                .setTitle(':x:خطاء')
+                .setAuthor(message.author.username, message.author.avatarURL)
+                .setColor("RANDOM")
+                .setDescription(`**${result.author.username}** الإجابة خاطئة`);
+                message.channel.sendEmbed(embedx);
+           }
+     });
+  }
+});
+client.login(process.env.BOT_TOKEN);
