6c2a9f0a396999decd96e08ed7c13cf642cd0ead
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2019-01-02 05:56:41 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -986,66 +986,45 @@ var trans =[
 
 
 ];
-client.on('message', message => {
-if (!points[message.author.id]) points[message.author.id] = {
-    points: 0,
-  };
-  if(!message.guild) return;
-    let id = message.author.id,prefix=".";
-    if (spee[id] && (new Date).getTime() - spee[id] < 15*1000) {
-        let r = (new Date).getTime() - spee[id];
-        r = 15*1000 - r;
-    }
-    if ( message.content == prefix+'فكك'){
-       
-        try{
-}catch(e){
- 
-}
- 
-    if(!message.channel.guild) return message.reply('**:no_entry: , هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
- 
- 
-const item = speed[Math.floor(Math.random() * speed.length)];
-const filter = response => {  
-    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-};
-message.channel.send('**:loudspeaker: , لقد بدأت اللعبة الان !**').then(msg => {
- 
- const embed = new Discord.RichEmbed()
- .setColor("RANDOM")
-     .setAuthor(`:timer: , لديك »15« ثانية فقط لكتابة هذه الكلمة`)
-          .setImage(`${item.type}`)
- .setFooter(`${message.author.tag}`, message.author.avatarURL)
- 
- 
-         
-msg.channel.send(embed).then(() => {
-        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
-        .then((collected) => {
-                  const sh = new Discord.RichEmbed()
-  .setColor("RANDOM")
-.setDescription('**:tada: , جيد , لقد حصلت على نقطة**')
-.addField('.اكتب نقاطي', 'لرؤية نقاطك' , true)
-.setFooter(message.author.username, message.author.avatarURL)
+    if(message.content == prefix+"ترجمة"){
+        if(UserBlocked.has(message.guild.id)) return message.channel.send("هناك جلسة .")
+        UserBlocked.add(message.guild.id)
+        var ask = trans[Math.floor(Math.random() * trans.length)];
+        let embed = new Discord.RichEmbed()
+        .setTitle('فكك الكلمة بسرعة')
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setColor("RANDOM")
+        .setDescription(ask.q);
+        message.channel.sendEmbed(embed).then(msg=> msg.delete(20000))
+        const msgs = await message.channel.awaitMessages(msg => msg.author.id !== client.user.id ,{maxMatches:1,time:10000});
+            UserBlocked.delete(message.guild.id)
+        msgs.forEach(result => {
+           if(result.author.id == client.user.id) return;
+           if(result.content == "ترجمة") return
+           if(result.content == ask.a){
+             let embeds = new Discord.RichEmbed()
+             .setTitle(':white_check_mark: اجابة صحيحة')
+             .setAuthor(message.author.username, message.author.avatarURL)
+             .setColor("RANDOM")
+             .setDescription(`**${result.author.username}** الإجابة صحيحة`);
 message.channel.sendEmbed(sh);
             let won = collected.first().author;
             points[won.id].points++;
-          })
-          .catch(collected => {
-            message.channel.send(`**:timer: , انتهئ الوقت , ولم يقم احد بتفكيك الكلمة**`);
-          })
           fs.writeFile("./points.json", JSON.stringify(points), (err) => {
     if (err) console.error(err)
-          })
-        })
-    })
-    spee[id] = (new Date).getTime()
-}
+                message.channel.sendEmbed(embeds);                return;
+           } else {
+ 
+                                  var embedx = new Discord.RichEmbed()
+                .setTitle(':x:خطأ')
+                .setAuthor(message.author.username, message.author.avatarURL)
+                .setColor("RANDOM")
+                .setDescription(`**${result.author.username}** الإجابة خاطئة`);
+                message.channel.sendEmbed(embedx);
+           }
+     });
+  }
 });
-
-
-
 
 
 
